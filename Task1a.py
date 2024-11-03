e024af6bc125819aa9602fbeefe24f2fc8029247
dmliddle <david.liddle@duke.edu>
2024-11-03 18:02:03 -0500

    Complete Task 1a

--- Task1a.py
+++ Task1a.py
@@ -1,15 +1,27 @@
-# Import modules
-import sys, os, arcpy
+# Import necessary modules
+import sys
+import os
+import arcpy
 
 # Set workspace
 arcpy.env.workspace = "V:\ENV859_PS4\Data"
-# Set variables
-inputFile = 'V:\ENV859_PS4\Data\Streams.shp'
-out_feature_class = 'V:\ENV859_PS4\Scratch\StrmBuff1km.shp'
-# Set output variable
 
-# Second string variable that sets distance to 1000 meters
+# Define the path to the input Streams.shp feature class
+inputFile = 'V:\ENV859_PS4\Data\Streams.shp'
+
+# Define the buffer distance as "1000 meters"
 buff_dist = "1000 meters"
 
+# Define the path and filename for the output buffer feature class in the Scratch folder
+out_feature_class = 'V:\ENV859_PS4\Scratch\StrmBuff1km.shp'
 
+# Execute the buffer command
 
+arcpy.Buffer_analysis(inputFile, out_feature_class, buff_dist,
+    line_side="FULL",
+    line_end_type="ROUND",
+    dissolve_option="ALL"
+)
+
+# Display any warnings or errors
+print(arcpy.GetMessages())
