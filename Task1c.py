c174da52eec8d18200bee3b7e6ce3f60dc3ae35d
dmliddle <david.liddle@duke.edu>
2024-11-03 20:57:29 -0500

    Task1c

---
+++ Task1c.py
@@ -0,0 +1,22 @@
+# Import necessary modules
+import sys
+import arcpy
+
+# Set workspace
+arcpy.env.workspace = "V:\ENV859_PS4\Data"
+arcpy.env.overwriteOutput = True
+
+# Define the path to the input Streams.shp feature class (file name only)
+inputFile = "Streams.shp"
+
+# Get user input for buffer distance and output file path
+buffer_dist = arcpy.GetParameterAsText(0)  # e.g., "1000"
+buffer_dist_2 = buffer_dist + " meters"
+out_feature_class = arcpy.GetParameterAsText(1) 
+
+# Execute the buffer command
+
+arcpy.Buffer_analysis(inputFile, out_feature_class, buffer_dist_2,"FULL","ROUND", "ALL")
+
+# Display any warnings or errors
+print(arcpy.GetMessages())
